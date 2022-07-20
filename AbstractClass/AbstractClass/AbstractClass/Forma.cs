912aaf79bde58455376e296f971ef2c9972c1b1f
Brenno Graça <brennosg_3712@hotmail.com>
2022-07-20 14:42:11 -0300

    Implementing abstract class

---
+++ AbstractClass/AbstractClass/AbstractClass/Forma.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    //Abstract classes canno't be implemented only inherited
+    public abstract class Forma
+    {
+        public string Cor { get; set; }
+        public double Area { get; set; }
+        public double Perimetro { get; set; }
+
+        public abstract void CalcularArea();
+        public abstract void CalcularPerimetro();
+
+        public string Descricao()
+        {
+            return "Sou uma classe abstrata Forma";
+        }
+    }
+}
--- AbstractClass/AbstractClass/AbstractClass/Program.cs
+++ AbstractClass/AbstractClass/AbstractClass/Program.cs
@@ -10,6 +10,17 @@ namespace AbstractClass
     {
         static void Main(string[] args)
         {
+            Quadrado q = new Quadrado();
+
+            Console.Write("Informe o valor do lado do quadrado em metros: ");
+            q.Lado = Convert.ToInt32(Console.ReadLine());
+
+            q.CalcularArea();
+            q.CalcularPerimetro();
+
+            Console.WriteLine("A área do quadrado é de {0} m²", q.Area);
+            Console.WriteLine("O perímetro do quadrado é de {0} m²", q.Perimetro);
+
             Console.ReadLine();
         }
     }
